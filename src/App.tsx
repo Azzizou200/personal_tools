c3b92e6e81f38bde84213d15fdfe0526d9afbac5
azzeddine <azzizou200@gmail.com>
2026-09-14 20:33:25 +0100

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Walid is a bitch</h1>
+          <h1>sava chwiya
+          </h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
